493f6da581972d3b75f07da7e2d1c7d6c9817872
keshavBajpai-web <kehavbajpai11@gmail.com>
2026-09-20 16:25:55 +0530

    update middleware

--- backend/router/jobRoute.js
+++ backend/router/jobRoute.js
@@ -5,7 +5,7 @@ import { isRecruiter } from '../middlewares/isRecruiter.js';
 
 const router = express.Router()
 
-router.route("/post").post(isAuthenticate,isRecruiter,postJob);
+router.route("/post").post(isAuthenticated,isRecruiter,postJob);
 router.route("/getAllJobs").get(getAllJob);
 router.route("/getAdminJobs").get(isAuthenticated,getAdminJob);
 router.route("/get/:id").get(getJobById);
